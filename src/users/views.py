9915e11f5d161d30c7938d21bae736ebf2064952
exite-off <vfedin868@gmail.com>
2026-01-01 21:56:47 +0200

    Password recovery feature KAN-15 realization. Implemented 4 views and 2 forms. Also created templates for the 4 views. Setted up django built-in smtp server for recovery link sending.

--- src/users/views.py
+++ src/users/views.py
@@ -4,23 +4,24 @@ from .forms import RegistrationForm
 
 
 def register_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
+            user.set_password(form.cleaned_data["password"])
             user.save()
             login(request, user)
-            return redirect('home')
+            return redirect("home")
     else:
         form = RegistrationForm()
 
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, template_name="users/register.html", context={"form": form})
 
 
 def terms_view(request):
-    return render(request, 'users/terms.html')
+    return render(request, template_name="users/terms.html")
 
 
 def login_view(request):
-    return render(request, 'users/login.html')
+    return render(request, template_name="users/login.html")
+
